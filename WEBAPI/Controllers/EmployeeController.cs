4ba95ce76e16772df183d45e3a4145a67aedf6b7
Muhammad Waleed <pythoning7@gmail.com>
2022-02-26 18:43:51 +0500

    Initial Repo

---
+++ WEBAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBAPI.ViewModel;
+
+namespace WEBAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+       
+        [HttpPost]
+        [Route("Add-Emp")]
+        public EmployeeVM addEmployee(EmployeeVM EMPVM)
+        {
+            return EMPVM;
+        }
+        
+    }
+}
